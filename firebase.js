1eb20e7acf82963366cff9d82ad07e4d28249a23
Ilhomjon <futurewithme1221@gmail.com>
2025-01-20 12:54:01 +0500

    depc: Installed firebase for server and hosting app

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'xxxxxx',
+  authDomain: 'xxxxx',
+  projectId: 'xxxxx',
+  storageBucket: 'xxxxx',
+  messagingSenderId: 'xxxxx',
+  appId: 'xxxxx',
+  measurementId: 'xxxxx',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export { db }
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createWebHistory, createRouter } from 'vue-router'
 import HomeView from '../pages/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
@@ -12,11 +12,6 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/pages/About.vue'),
     },
     {
       path: '/shopping',
