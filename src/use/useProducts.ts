9dc9884c316e317b0b49a8182fd64bc9b4f56e41
Ilhomjon <futurewithme1221@gmail.com>
2025-01-29 21:03:41 +0500

    refactor: Refactored the way creating useProducts composable function

--- src/use/useProducts.ts
+++ src/use/useProducts.ts
@@ -1,34 +1,30 @@
-import { computed, onMounted, ref } from 'vue'
+import { computed, ref } from 'vue'
 import { db } from '../../firebase.js'
 import { doc, getDoc } from 'firebase/firestore'
 import type { ProductType } from '@/core/index.js'
 
-export function useProducts() {
-  const _data = ref<{ products: ProductType[] }>({ products: [] })
-  const _error = ref<Error>()
-  const _loading = ref(false)
+const _data = ref<{ products: ProductType[] }>({ products: [] })
+const _error = ref<Error>()
+const _loading = ref(false)
 
-  const loading = computed(() => _loading.value)
-  const data = computed(() => _data.value)
+const loading = computed(() => _loading.value)
+const data = computed(() => _data.value)
 
-  const getProducts = async () => {
-    _loading.value = true
-    try {
-      const docRef = doc(db, 'products', 'jyvzvvrraYT9mTTO3Aqp')
-      const res = await getDoc(docRef)
-      _data.value = res.data()?.products
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        _error.value = err
-      }
-    } finally {
-      _loading.value = false
+const getProducts = async () => {
+  _loading.value = true
+  try {
+    const docRef = doc(db, 'products', 'jyvzvvrraYT9mTTO3Aqp')
+    const res = await getDoc(docRef)
+    _data.value = res.data()?.products
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      _error.value = err
     }
+  } finally {
+    _loading.value = false
   }
-
-  onMounted(() => {
-    getProducts()
-  })
-
-  return { loading, data }
 }
+
+getProducts()
+
+export const useProducts = () => ({ loading, data })
